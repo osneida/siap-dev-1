4c43eecc8779c9ab6641fff1c6afce5f1004bf3e
Administrador <hbenda.siap@gmail.com>
2018-11-28 11:12:59 -0600

    Carga de la carpetas app/resources/routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use App\Menu;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      /* herlbeng, 24-11-2018 */
+        View::share('menus', Menu::menus());
+        view()->composer(['home','propietarios.*','welcome'], function($view) {
+        $view->with('menus', Menu::menus());
+      });
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,49 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+/*Route::get('/', function () {
+    return 'Home';
+});*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@home');
+
+
+Route::get('/plantillacorreos', 'PlantillacorreoController@index')
+        ->name('plantillacorreos.index');
+
+Route::get('/plantillacorreos/{plantillacorreo}', 'PlantillacorreoController@show')
+        ->where('plantillacorreo','\d+')
+        ->name('plantillacorreos.show');
+
+Route::get('/plantillacorreos/nuevo', 'PlantillacorreoController@create')
+                ->name('plantillacorreos.create');
+
+Route::get('/plantillacorreos/{plantillacorreo}/editar', 'PlantillacorreoController@edit')
+                ->name('plantillacorreos.edit');
+
+Route::put('/plantillacorreos/{plantillacorreo}', 'PlantillacorreoController@update');
+
+Route::post('/plantillacorreos', 'PlantillacorreoController@store');
+
+Route::delete('/plantillacorreos/{plantillacorreo}', 'PlantillacorreoController@destroy')
+        ->name('plantillacorreos.destroy');
+
+
+Route::get('/propietarios', 'PropietarioController@index')
+        ->name('propietarios.index');
+
+Route::get('/propietarios/{propietario}', 'PropietarioController@show')
+        ->where('propietario','\d+')
+        ->name('propietarios.show');
+
+Route::get('/propietarios/nuevo', 'PropietarioController@create')
+                ->name('propietarios.create');
+
+Route::get('/propietarios/{propietario}/editar', 'PropietarioController@edit')
+                ->name('propietarios.edit');
+
+Route::put('/propietarios/{propietario}', 'PropietarioController@update');
+
+Route::post('/propietarios', 'PropietarioController@store');
+
+Route::delete('/propietarios/{propietario}', 'PropietarioController@destroy')
+        ->name('propietarios.destroy');
